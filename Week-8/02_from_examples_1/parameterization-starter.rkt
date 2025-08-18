1d542795054450f95298425d62efe7319e56d66b
sternness1783 <>
2025-08-19 00:08:34 +0900

    update

--- Week-8/02_from_examples_1/parameterization-starter.rkt
+++ Week-8/02_from_examples_1/parameterization-starter.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname parameterization-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname parameterization-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; parameterization-starter.rkt
 
 (* pi (sqr 4)) ;area of circle radius 4
